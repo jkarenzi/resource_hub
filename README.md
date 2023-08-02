a39d935bdb5becf99f0486924e4e88aef496209e
jkarenzi <j.karenzi@alustudent.com>
2023-08-02 12:10:33 +0200

    changes

--- README.md
+++
@@ -1,6 +0,0 @@
-# ResourceHub
-
-## requirements
-
-* flask
-* pymysql
--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-function toggleCommentForm(postId) {
-    var commentForm = document.getElementById("comment-form-" + postId);
-    if (commentForm.style.display === "none") {
-        commentForm.style.display = "block";
-    } else {
-        commentForm.style.display = "none";
-    }
-}
